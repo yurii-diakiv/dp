fc5f1a64ac838233cd949a8c6f169193a5f175d6
diakiv-yurii <yurii.diakiv@binary-studio.com>
2021-12-12 01:20:35 +0200

    DP-16: add exceptions

---
+++ frontend/src/exceptions/http-error/http-error.exception.js
@@ -0,0 +1,19 @@
+import { HttpCode, CustomExceptionName } from 'common/enums';
+import { DEFAULT_MESSAGE } from './common/constants';
+
+class HttpError extends Error {
+  status;
+  messages;
+
+  constructor({
+    status = HttpCode.INTERNAL_SERVER_ERROR,
+    messages = [DEFAULT_MESSAGE],
+  } = {}) {
+    super(messages.join());
+    this.status = status;
+    this.messages = messages;
+    this.name = CustomExceptionName.HTTP_ERROR;
+  }
+}
+
+export { HttpError };
