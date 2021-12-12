40d873ccd705f3cca9a33ad6de026a5685412b38
yurii-diakiv <yuradyakiv@gmail.com>
2021-12-12 13:17:05 +0200

    DP-14: add notification service

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,10 +1,13 @@
 import { Http } from './http/http.service';
 import { Storage } from './storage/storage.service';
+import { Notification } from './notification/notification.service';
 
 const http = new Http();
 
+const notification = new Notification();
 const storage = new Storage({ storage: localStorage });
 
 export {
-    storage
+    storage,
+    notification
 };
