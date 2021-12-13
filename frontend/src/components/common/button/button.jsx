705419267fc980e5d850464aec250ce6f5d9afc5
yurii-diakiv <yuradyakiv@gmail.com>
2021-12-14 01:08:44 +0200

    DP-5: add icon button styles

--- frontend/src/components/common/button/button.jsx
+++ frontend/src/components/common/button/button.jsx
@@ -2,9 +2,9 @@ import clsx from 'clsx';
 
 import styles from './styles.module.scss';
 
-const Button = ({ type, label, onClick, color }) => (
+const Button = ({ type, label, onClick, color, round, iconBtn }) => (
     <button
-        className={clsx(styles.btn, styles[color])}
+        className={clsx(styles.btn, styles[color], iconBtn && styles.iconBtn, round && styles.round)}
         type={type}
         onClick={onClick}
     >
