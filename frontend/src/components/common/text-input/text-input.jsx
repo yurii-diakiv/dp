d19aae3c73d8a768ebd867c6cbaa3ec4ebff9fd2
yurii-diakiv <yuradyakiv@gmail.com>
2021-12-14 01:08:06 +0200

    DP-5: make label optional, add default value and onChange, onBlur events to input

--- frontend/src/components/common/text-input/text-input.jsx
+++ frontend/src/components/common/text-input/text-input.jsx
@@ -2,17 +2,20 @@ import clsx from 'clsx';
 
 import styles from '../common-styles.module.scss';
 
-const TextInput = ({ type, label, placeholder, color, disabled, field }) => (
+const TextInput = ({ type, label, placeholder, color, disabled, onChange, onBlur, defaultValue, field }) => (
     <span className={styles.inputControl}>
         <label className={styles.label}>
-            <span className={clsx(styles.labelText)}>
+            {label && <span className={clsx(styles.labelText)}>
                 {label}
-            </span>
+            </span>}
             <input
                 type={type}
                 placeholder={placeholder}
                 className={clsx(styles.textInput, styles[color])}
                 disabled={disabled}
+                onChange={onChange}
+                onBlur={onBlur}
+                defaultValue={defaultValue}
                 {...field}
             />
         </label>
